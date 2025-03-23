cc238241000319c17f14a1adcbb614c20ac9179d
Naqeeb <ahmadzan@mcmaster.ca>
2025-03-23 18:47:15 -0400

    fixing again

---
+++ Coordinate.java
@@ -0,0 +1,21 @@
+package ca.mcmaster.se2aa4.island.teamXXX;
+
+public class Coordinate {
+
+    private int x;
+    private int y;
+
+    public Coordinate(int xCoordinate, int yCoordinate){
+        this.x = xCoordinate;
+        this.y = yCoordinate;
+    }
+    
+    public int getXCoordinate(){
+        return x;
+    }
+
+    public int getYCoordinate(){
+        return y;
+    }
+    
+}
