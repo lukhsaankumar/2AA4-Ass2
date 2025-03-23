cbfb6de1a1ba06ae69a5db121afdc51446291a14
Nivii177 <desilm5@mcmaster.ca>
2025-03-22 22:28:58 -0400

    update

---
+++ src/main/java/ca/mcmaster/se2aa4/island/teamXXX/coordinate.java
@@ -0,0 +1,21 @@
+package ca.mcmaster.se2aa4.island.teamXXX;
+
+public class coordinate {
+
+    private int x;
+    private int y;
+
+    public coordinate(int xCoordinate, int yCoordinate){
+        this.x = xCoordinate;
+        this.y = yCoordinate;
+    }
+    
+    public int getXCoordinate(){
+        return x;
+    }
+
+    public int getYCoordinate(){
+        return y;
+    }
+    
+}
