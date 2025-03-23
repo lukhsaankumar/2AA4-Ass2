73d7a38bdd4d7073a43fe0536962a30702aa6f93
Nivii177 <desilm5@mcmaster.ca>
2025-03-22 21:23:35 -0400

    Updated Drone constructor

--- src/main/java/ca/mcmaster/se2aa4/island/teamXXX/Drone.java
+++ src/main/java/ca/mcmaster/se2aa4/island/teamXXX/Drone.java
@@ -3,8 +3,14 @@ package ca.mcmaster.se2aa4.island.teamXXX;
 import org.json.JSONObject;
 
 public class Drone{
+
+    private Compass inDir;
+    private Compass direct;
     
     public Drone(Integer charge, String dir){
-
-}
+        Compass compass = Compass.NORTH;
+        Compass direction  = compass.StringToCompass(dir);
+        this.inDir = direction;
+        this.direct = direction;
+    }
 }
