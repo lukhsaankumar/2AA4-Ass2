3f1068494a717eb5bd075187e3fe1436004d56c5
Naqeeb <ahmadzan@mcmaster.ca>
2025-03-23 21:49:51 -0400

    small changes

--- src/main/java/ca/mcmaster/se2aa4/island/team46/Enums/Direction.java
+++ src/main/java/ca/mcmaster/se2aa4/island/team46/Enums/Direction.java
@@ -1,7 +1,8 @@
 package ca.mcmaster.se2aa4.island.team46.Enums;
 
 public class Direction {
+    
     public enum Compass {
         N, E, W, S
-}
+    }
 }
--- src/main/java/ca/mcmaster/se2aa4/island/team46/Interface/Finder.java
+++ src/main/java/ca/mcmaster/se2aa4/island/team46/Interface/Finder.java
@@ -1,6 +1,5 @@
 package ca.mcmaster.se2aa4.island.team46.Interface;
 
-import org.json.JSONObject;
 
 public abstract class Finder {
     
