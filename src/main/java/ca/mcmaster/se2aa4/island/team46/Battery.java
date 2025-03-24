a1680e819ca0a9fc270bd1908053cfe85430ef60
Naqeeb <ahmadzan@mcmaster.ca>
2025-03-24 00:10:55 -0400

    Implemented the battery class (closes #14)

--- src/main/java/ca/mcmaster/se2aa4/island/team46/Battery.java
+++ src/main/java/ca/mcmaster/se2aa4/island/team46/Battery.java
@@ -5,22 +5,25 @@ public class Battery {
     private int charge;
     private int initialCharge;
 
+
     public Battery(int initialCharge){
         this.initialCharge = initialCharge;
         this.charge  = initialCharge;
     }
+
 
     public int drainBattery(int cost){
         this.charge -= cost;
         return this.charge;
     }
 
+
     public int getInitialCharge(){
         return initialCharge;
     }
 
+
     public int getCharge(){
         return charge;
     }
-    
 }
