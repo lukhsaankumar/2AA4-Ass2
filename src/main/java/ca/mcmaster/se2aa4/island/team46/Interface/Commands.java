c6e7ff29439c77a67ff9b7d43c71e5b5762d3694
Naqeeb <ahmadzan@mcmaster.ca>
2025-03-23 21:55:16 -0400

    finalized the interface implementation (close #8)

--- src/main/java/ca/mcmaster/se2aa4/island/team46/Interface/Commands.java
+++ src/main/java/ca/mcmaster/se2aa4/island/team46/Interface/Commands.java
@@ -3,6 +3,7 @@ package ca.mcmaster.se2aa4.island.team46.Interface;
 import org.json.JSONObject;
 
 public interface Commands {
+    
     public JSONObject getLastCommand();
 
     public void addCommand(JSONObject command);
--- src/main/java/ca/mcmaster/se2aa4/island/team46/Interface/Finder.java
+++ src/main/java/ca/mcmaster/se2aa4/island/team46/Interface/Finder.java
@@ -1,7 +1,20 @@
 package ca.mcmaster.se2aa4.island.team46.Interface;
 
+import org.json.JSONObject;
 
 public abstract class Finder {
-    
+
+    private Commands observer;
+
+
+        protected void update(JSONObject decision) {
+            if (observer != null) {
+                observer.addCommand(decision);
+            }
+        }
+
+        public void addObserver(Commands observer) {
+            this.observer = observer;
+        }
 
 }
