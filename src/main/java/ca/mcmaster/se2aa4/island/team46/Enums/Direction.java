f1b5ed19e4edd63ae2d1d18480c8b937225bde8a
Nivii177 <desilm5@mcmaster.ca>
2025-03-23 21:41:32 -0400

    Creation of Direction Enum (close #7)

---
+++ src/main/java/ca/mcmaster/se2aa4/island/team46/Enums/Direction.java
@@ -0,0 +1,7 @@
+package ca.mcmaster.se2aa4.island.team46.Enums;
+
+public class Direction {
+    public enum Compass {
+        N, E, W, S
+}
+}
--- src/main/java/ca/mcmaster/se2aa4/island/team46/Interface/Move.java
+++ src/main/java/ca/mcmaster/se2aa4/island/team46/Interface/Move.java
@@ -1,1 +1,18 @@
 package ca.mcmaster.se2aa4.island.team46.Interface;
+
+import ca.mcmaster.se2aa4.island.team46.Enums.Direction;
+
+
+public interface Move {
+
+    public void moveForward();
+
+    public void turnLeft();
+
+    public void turnRight();
+
+    public void stop();
+
+    public Direction.Compass getDirection();
+}
+
