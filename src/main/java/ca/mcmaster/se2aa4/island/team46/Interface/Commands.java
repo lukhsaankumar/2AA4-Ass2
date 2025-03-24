fb24e28df368cfc98c4f9f56fb344a97abc47a74
Nivii177 <desilm5@mcmaster.ca>
2025-03-23 21:51:03 -0400

    General Command setters and getter functions implemented

--- src/main/java/ca/mcmaster/se2aa4/island/team46/Interface/Commands.java
+++ src/main/java/ca/mcmaster/se2aa4/island/team46/Interface/Commands.java
@@ -1,2 +1,10 @@
 package ca.mcmaster.se2aa4.island.team46.Interface;
 
+import org.json.JSONObject;
+
+public interface Commands {
+    public JSONObject getLastCommand();
+
+    public void addCommand(JSONObject command);
+
+}
